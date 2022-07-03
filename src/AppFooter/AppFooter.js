62c99e461fb121f59c1acaca0e27c23285ae6a36
jryambao <johnralph@gmail.com>
2022-07-03 19:25:00 +0800

    added contact

--- src/AppFooter/AppFooter.js
+++ src/AppFooter/AppFooter.js
@@ -2,8 +2,13 @@ import React from 'react'
 import './AppFooter.css';
 const AppFooter = () => {
   return (
-    <div>
-    
+    <div className='container'>
+    <h1>Contact</h1>
+    <div className="icons">
+    <i class="fa-brands fa-github"></i>
+    <i class="fa-brands fa-facebook"></i>
+    <i class="fa-brands fa-instagram"></i>
+    </div>
     </div>
   )
 }
